2770808265992d55ab19d257875ce5fbf1ebc70c
Greg Breen <gregbree@amazon.com>
2026-09-22 16:30:30 +0800

    GH-63 Upgrade CDK lib to 2.267.0

--- cicd/test/cicd.test.ts
+++ cicd/test/cicd.test.ts
@@ -1,7 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import * as cdk from 'aws-cdk-lib';
+import * as cdk from 'aws-cdk-lib/core';
 import { Template } from 'aws-cdk-lib/assertions';
 import * as Cicd from '../lib/cicd-stack';
 
